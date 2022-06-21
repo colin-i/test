29f0c779ce40bbab8c740d6937bee4d672380966
bc <costin.botescu@gmail.com>
2022-06-21 15:49:25 +0300

    unpublisheds are in buildrec

---
+++ lp2.py
@@ -0,0 +1,21 @@
+
+
+from launchpadlib.launchpad import Launchpad
+
+import sys
+
+PPAOWNER = "colin-i"
+PPA = sys.argv[1]
+
+cachedir = "~/.launchpadlib/cache/"
+lp_ = Launchpad.login_anonymously('ppastats', 'production', cachedir)
+owner = lp_.people[PPAOWNER]
+archive = owner.getPPAByName(name=PPA)
+
+bs=archive.getBuildRecords()
+for b in bs:
+	if b.source_package_name==sys.argv[2] and b.source_package_version==sys.argv[3]:
+		if len(sys.argv)>4:
+			print(b.buildstate)
+		print(b.self_link)
+		break
