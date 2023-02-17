fef7cda1893b684dc4c659bdb0d9d770cdc5324f
bc <costin.botescu@gmail.com>
2023-02-17 09:19:37 +0200

    day/night terminal

---
+++ misc/is_day.php
@@ -0,0 +1,12 @@
+<?php
+$c=file_get_contents("/home/bc/n/pat4");
+$c=preg_split('/,/',$c);
+$t=time();
+$a=date_sun_info($t,$c[0],$c[1]);
+if($argc==2){
+	foreach ($a as $key => $val) {
+		echo "$key: " . date("H:i:s", $val) . "\n";
+	}
+}
+if($a['sunrise']<=$t && $t<=$a['sunset'])echo "1";
+?>
