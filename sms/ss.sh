12a3636fde4ab82003e50b11ae3b5664276bb0dc
bc <costin.botescu@gmail.com>
2023-03-11 21:10:53 +0200

    and

--- sms/ss.sh
+++ sms/ss.sh
@@ -2,16 +2,15 @@
 
 #1 text 2 nr
 
-a=`sudo mmcli -m 0 --messaging-create-sms="text='${1}',number='+40${2}'"`
-echo ${a}
 #Successfully created new SMS: /org/freedesktop/ModemManager1/SMS/0
-b=`echo ${a} | cut -d' ' -f5 | cut -d'/' -f6`
 
-a=`date '+%s'`
-f=/home/bc/Desktop/sms/$a.txt
-mmcli -m 0 -s ${b} > ${f}
-cat ${f}
-read -n1 kbd
-
-sudo mmcli -s ${b} --send
+a=`sudo mmcli -m 0 --messaging-create-sms="text='${1}',number='+40${2}'"` && \
+echo ${a} && \
+b=`echo ${a} | cut -d' ' -f5 | cut -d'/' -f6` && \
+a=`date '+%s'` && \
+f=/home/bc/Desktop/sms/$a.txt && \
+mmcli -m 0 -s ${b} > ${f} && \
+cat ${f} && \
+read -n1 kbd && \
+sudo mmcli -s ${b} --send && \
 sudo mmcli -m 0 --messaging-delete-sms=${b}
