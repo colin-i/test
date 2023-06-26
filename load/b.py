a9cbefc8d062e8a1f13e4d9d673981be97ab7750
bc <costin.botescu@gmail.com>
2023-06-26 09:17:27 +0300

    readchar

--- load/b.py
+++ load/b.py
@@ -1,4 +1,4 @@
-
+"""
 class bcolors:
     red = '\033[101m'
     green = '\033[102m'
@@ -6,22 +6,36 @@ class bcolors:
     end = '\033[0m'
     bold = '\033[1m'
     underline = '\033[4m'
-#print(f"{bcolors.bold}{bcolors.underline}{bcolors.red}1{bcolors.green}2{bcolors.yellow}3{bcolors.end}123")
-#print(f"{bcolors.bold}{bcolors.underline}{bcolors.red}1{bcolors.green}2{bcolors.yellow}3")
-#print("123")
-print(f"{bcolors.bold}{bcolors.underline}{bcolors.red} {bcolors.green} {bcolors.yellow} {bcolors.end}")
+#{bcolors.bold}{bcolors.underline}
+print(f"{bcolors.red} {bcolors.green} {bcolors.yellow} {bcolors.end}")  #if not end will continue at next print
 
 import os
 print(os.get_terminal_size().columns)
-
+"""
 
 from multiprocessing.connection import Listener
 
 address = ('192.168.1.15', 6000)     # family is deduced to be 'AF_INET'
 listener = Listener(address)
 
+def t2_f():
+	try:
+		while True:
+			conn = listener.accept()
+			print(conn.recv())
+			conn.close()
+	except Exception:
+		print("closed")
+
+import threading
+t2 = threading.Thread(target=t2_f)
+t2.start()
+
 while True:
-	conn = listener.accept()
-	#print('connection accepted from', listener.last_accepted)
-	print(conn.recv())
-	conn.close()
+	import readchar
+	c=readchar.readchar()
+	if c=='q':
+		break
+listener.close()   #this will close after accept gets next client
+print("will close")
+#if there are threads python will not exit
