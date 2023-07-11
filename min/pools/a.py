5cea7910f6c7933ed36f35aeced93c6982ba3e79
bc <costin.botescu@gmail.com>
2023-07-11 13:46:35 +0300

    min pools sort and show

--- min/pools/a.py
+++ min/pools/a.py
@@ -1,10 +1,16 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Firefox()
 driver.get("http://miningpoolstats.stream")
 coins = driver.find_element(By.ID, 'coins')
-print(coins.text)
+
+#sort by pools
+elem=coins.find_element(By.TAG_NAME,'thead').find_element(By.TAG_NAME,'tr').find_element(By.CLASS_NAME,'show1150')
+elem.send_keys(Keys.RETURN)
+
+with open("a.txt","w") as f:
+	f.write(coins.text)
 driver.close()
