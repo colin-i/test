5517454b60314a6b688b40814ef8baf1dfff36fd
bc <costin.botescu@gmail.com>
2023-08-25 22:53:41 +0300

    shared is no longer working, download is the alternative

--- google/google.py
+++ google/google.py
@@ -1,21 +1,17 @@
 
 #same as ./a
 
-from __future__ import print_function
+import sys
+import io
 
 import google.auth
 from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload
-
-import sys
+from googleapiclient.http import MediaIoBaseDownload
 
 mim='application/vnd.oasis.opendocument.spreadsheet'
 
 def upload_basic():
-	# create drive api client
-	service = build('drive', 'v3', credentials=creds)
-
 	file_metadata = {'name': fname} #, 'parents': ['###']
 
 	media = MediaFileUpload(fname,mimetype=mim)
@@ -23,20 +19,18 @@ def upload_basic():
 	file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 	print(F'File ID: {file.get("id")}')
 
-	yourEmailOfGoogleAccount = 'costin.botescu@gmail.com'
-	permission = {
-	    'type': 'user',
-	    'role': 'writer',
-	    'emailAddress': yourEmailOfGoogleAccount,
-	}
-	service.permissions().create(fileId=file['id'], body=permission).execute()
-
-	#file_back = service.files().get(fileId=file['id'], fields='webContentLink').execute()  # or fields='*'
-	#print(file_back.get('webContentLink'))
+	#"You cannot share this item because it has been flagged as inappropriate."
+	#yourEmailOfGoogleAccount = '***@gamil.com'
+	#permission = {
+	#    'type': 'user',
+	#    'role': 'writer',
+	#    'emailAddress': yourEmailOfGoogleAccount,
+	#}
+	#service.permissions().create(fileId=file['id'], body=permission).execute()
 
 	return file
 
-def search_file(newid):
+def search_file(newid,download=False):
 	#files = []
 	page_token = None
 	while True:
@@ -55,6 +49,15 @@ def search_file(newid):
 					if id!=newid:
 						service.files().delete(fileId=id).execute()
 						print('deleted')
+				elif download==True:
+					#file_back = service.files().get(fileId=file['id'], fields='webContentLink').execute()  # or fields='*'
+					request = service.files().get_media(fileId=file['id'])
+					file = io.FileIO(fname,'wb')
+					downloader = MediaIoBaseDownload(file, request)
+					done = False
+					while done is False:
+						status, done = downloader.next_chunk()
+						print(F'Download {int(status.progress() * 100)}.')
 
 		#files.extend(response.get('files', []))
 		page_token = response.get('nextPageToken', None)
@@ -62,12 +65,16 @@ def search_file(newid):
 			break
 
 creds, _ = google.auth.default()
+# create drive api client
 service = build('drive', 'v3', credentials=creds)
 
 fname=sys.argv[1]
 try:
-	sys.argv[2]
-	search_file(None)
+	flag=sys.argv[2]
+	if flag=="0":
+		search_file(None,True)
+	else:
+		search_file(None)
 except:
 	f=upload_basic()
 	search_file(f['id'])
