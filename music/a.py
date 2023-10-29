4f2932498b7cf3d1bacb41c1d936db41f76eba1e
bc <costin.botescu@gmail.com>
2023-10-29 15:03:24 +0200

    solfege keys

---
+++ music/a.py
@@ -0,0 +1,16 @@
+
+import readchar
+
+while True:
+	c=readchar.readchar()
+	if c=='q': break
+	elif c=='z': print('do')
+	elif c=='x': print('re')
+	elif c=='c': print('mi')
+	elif c=='v': print('fa')
+	elif c=='b': print('sol')
+	elif c=='n': print('la')
+	elif c=='m': print('si')
+	elif c==',': print('doh')
+
+#sound.setVolume(0-100) #default is 100
