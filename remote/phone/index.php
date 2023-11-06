cb4504c1df35030096fc4a7409c65aedaf9ab70e
bc <costin.botescu@gmail.com>
2023-11-06 14:02:40 +0200

    000.. can also skip the warning

---
+++ remote/phone/index.php
@@ -0,0 +1,23 @@
+
+<?php
+$curl = curl_init();
+$url="https://wondrous-thankfully-swift.ngrok-free.app/remote.php";
+curl_setopt_array($curl, array(
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+//CURLOPT_HTTPHEADER => [
+//    'ngrok-skip-browser-warning: 2'
+//]
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo($response);
+?>
--- remote/phone/remote.php
+++ remote/phone/remote.php
@@ -1,25 +1,20 @@
 
-<form method="GET">
-	<input type="submit" name="a">
-</form>
-
 <?php
-if(count($_GET)){
-	$fp = fopen('remote.txt', 'w');//must create,chmod__w the file first
-	fwrite($fp, "a");
-	fclose($fp);
+$fp = fopen('remote.txt', 'w');//must create,chmod__w the file first
+fwrite($fp, "a");
+fclose($fp);
 
-	$f='remote2.txt';
-	do{
-		sleep(5);
-	}while(filesize($f)==0);//same
-	$fp = fopen($f, 'r');
-	$a=fread($fp,20);
-	//$fp=freopen(NULL, "w", $fp);//undefined
-	//ftruncate($fp, 0);//not working (working only at php -f)
-	fclose($fp);
-	$fp = fopen($f, 'w');
-	fclose($fp);
-	echo($a);
-}
+$f='remote2.txt';
+do{
+	sleep(5);
+}while(filesize($f)==0);//same
+$fp = fopen($f, 'r');
+$a=fread($fp,20);
+//$fp=freopen(NULL, "w", $fp);//undefined
+//ftruncate($fp, 0);//not working (working only at php -f)
+fclose($fp);
+$fp = fopen($f, 'w');
+fclose($fp);
+echo($a);
+
 ?>
