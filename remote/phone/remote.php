79ec0af0f1909f2bbddbdb14eb06aac53fca0719
bc <costin.botescu@gmail.com>
2023-11-08 11:32:17 +0200

    clearstatcache

--- remote/phone/remote.php
+++ remote/phone/remote.php
@@ -1,21 +1,24 @@
+<body>
 <?php
 
 $fp = fopen('remote.txt', 'w');//must create,chmod__w the file first
 fwrite($fp, "a");
 fclose($fp);
 
-$f='remote2.txt';
-do{
-	sleep(5);
-}while(filesize($f)==0);//same
+$f='remote2.txt';//same
+while(stat($f)['size']==0){
+	sleep(1);
+	clearstatcache();
+}
 $fp = fopen($f, 'r');
 $a=fread($fp,20);
 //$fp=freopen(NULL, "w", $fp);//undefined
 //ftruncate($fp, 0);//not working (working only at php -f)
 fclose($fp);
+echo($a);
+
 $fp = fopen($f, 'w');
 fclose($fp);
-echo($a);
 
 $image = imagecreatefromjpeg('a.jpg');
 ob_start();
@@ -29,3 +32,4 @@ fwrite($fp, "a");
 fclose($fp);
 
 ?>
+</body>
