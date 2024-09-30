8ce51528d4faca986ec1d3b71a9db0ff86ca8f38
bc <costin.botescu@gmail.com>
2024-09-30 10:49:37 +0300

    upd with add

--- google/upd.py
+++ google/upd.py
@@ -1,18 +1,33 @@
 
 import os
+import readchar
 
-qr=os.environ.get('query_result')
-fname=qr.split(',')[0]
 fn="/home/bc/this.csv"
-with open(fn) as f:
-	text=f.read()
+def parse():
+	qr=os.environ.get('query_result')
+	fname=qr.split(',')[0]
+
+	global lines
+	with open(fn) as f:
+		text=f.read()
 	lines = text.splitlines()
 	n=len(lines)-1
 	for i in range(0,n):
 		if lines[i].split(',')[0]==fname:
 			print("updated")
 			lines[i]=qr
-			break
+			return
+	print("1 top, 2 bottom, else alpha")
+	c=readchar.readchar()
+	print(c)
+	if c=='1':
+		lines.insert(0,qr)
+	elif c=='2':
+		lines.append(qr)
+	else:
+		lines.append(qr)
+		lines.sort()
+parse()
 with open(fn,"w") as f:
 	for r in lines:
 		print(r,end='\n',file=f)
