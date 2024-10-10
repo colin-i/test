be80a7d9d80a0b053213167964a7a99d3180df54
bc <costin.botescu@gmail.com>
2024-10-10 15:33:49 +0300

    at measures

--- arcgis/a.py
+++ arcgis/a.py
@@ -1,9 +1,19 @@
 
+import os
+
+recs=[[1087,554,"A0_3"]]
+#bottom-left switch for watermark
+#with open(os.environ["HOME"]+"/measures/list","wb") as file:
+#	for r in recs:
+#		file.write(r[2])
+#exit()
+
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.by import By
+import readchar
 
 service = Service('/usr/bin/geckodriver')
 opts = Options()
@@ -16,10 +26,24 @@ action = ActionBuilder(driver)
 driver.get("https://cestrin.maps.arcgis.com/apps/webappviewer/index.html?id=210f9dcdbeaf48349e3ed19e92ee2f19")
 info=driver.find_element(By.TAG_NAME, 'body')
 
-info.find_element(By.CLASS_NAME,"jimu-checkbox").click()
+readchar.readchar() #next is async
+
+info.find_element(By.ID,"jimu_dijit_CheckBox_0").click()
+#this was working at first try: By.CLASS_NAME,"jimu-checkbox" ele.click()
+#now: Element  is not clickable at point  because another element  obscures it ? driver.execute_script("arguments[0].click();", element) . but is another checkbox
+# for searching: with open('q','w') as f: f.write(driver.page_source)
+
 info.find_element(By.CLASS_NAME,"jimu-btn").click()
 
-#action.pointer_action.move_to_location(600, 500)
-#action.pointer_action.click()
-#action.perform()
+def click(x,y):
+	action.pointer_action.move_to_location(x,y);action.pointer_action.click();action.perform()
+	readchar.readchar() #async
+
+for r in recs:
+	click(r[0],r[1]) #to load for write
+	with open(os.environ["HOME"]+"/measures/"+r[2]+"x","wb") as file:
+		file.write(info.text.encode())
+	action.pointer_action.move_to_location(500,500);action.pointer_action.click();action.perform()
+exit(0)
+# action.pointer_action.move_to_location(,);action.pointer_action.click();action.perform()
 #info.text
