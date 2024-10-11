c53a84cd4bd1a0f38543105bd77436d7d6adcbfb
bc <costin.botescu@gmail.com>
2024-10-11 11:45:15 +0300

    clicks at gis

--- arcgis/a.py
+++ arcgis/a.py
@@ -1,20 +1,11 @@
 
-import os
-
-recs=[[1087,554,"A0_3"]]
-#bottom-left switch for watermark
-# action.pointer_action.move_to_location(,);action.pointer_action.click();action.perform()
-#with open(os.environ["HOME"]+"/measures/list","wb") as file:
-#	for r in recs:
-#		file.write(r[2])
-#exit()
-
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.by import By
 import readchar
+import os
 
 service = Service('/usr/bin/geckodriver')
 opts = Options()
@@ -33,16 +24,24 @@ info.find_element(By.ID,"jimu_dijit_CheckBox_0").click()
 #this was working at first try: By.CLASS_NAME,"jimu-checkbox" ele.click()
 #now: Element  is not clickable at point  because another element  obscures it ? driver.execute_script("arguments[0].click();", element) . but is another checkbox
 # for searching: with open('q','w') as f: f.write(driver.page_source)
-
 info.find_element(By.CLASS_NAME,"jimu-btn").click()
+
+#turn of additional map markers
+info.find_element(By.ID,"jimu_dijit_CheckBox_1").click()
 
 def click(x,y):
 	action.pointer_action.move_to_location(x,y);action.pointer_action.click();action.perform()
 	readchar.readchar() #async
 
+root=os.environ["HOME"]+"/measures/"
+with open(root+"recs","rb") as file:
+	recs=eval(file.read())
+#bottom-left switch for watermark
+# action.pointer_action.move_to_location(,);action.pointer_action.click();action.perform()
+
 for r in recs:
 	click(r[0],r[1]) #to load for write
-	with open(os.environ["HOME"]+"/measures/"+r[2]+"x","wb") as file:
+	with open(root+r[2]+"x","wb") as file:
 		file.write(info.text.encode())
 	action.pointer_action.move_to_location(500,500);action.pointer_action.click();action.perform()
 exit(0)
