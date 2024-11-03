4f5d60c0c89512116689698986708f46a110e837
bc <costin.botescu@gmail.com>
2024-11-03 12:48:11 +0200

    arcgis: diff %, pack _new

--- arcgis/a.py
+++ arcgis/a.py
@@ -34,18 +34,24 @@ def click(x,y):
 	readchar.readchar() #async
 
 root=os.environ["HOME"]+"/measures/"
-with open(root+"recs","rb") as file:
-	recs=eval(file.read())
-#bottom-left switch for watermark
-# !q=[];action.pointer_action.move_to_location(q[0],q[1]);action.pointer_action.click();action.perform()
-# with open(root+"new/","wb") as file: file.write(info.text.encode())
-for r in recs:
-	if len(r)>3:
-		extra=root+"ex/"+r[2]
-		if not os.path.isfile(extra):
-			with open(extra,"w") as file:
-				file.write(r[3])
-	click(r[0],r[1]) #to load for write
-	with open(root+"new/"+r[2],"wb") as file: file.write(info.text.encode())
-	action.pointer_action.move_to_location(500,500);action.pointer_action.click();action.perform()
+
+def pack(type):
+	with open(root+"recs"+type,"rb") as file:
+		rd=file.read()
+		print(rd)
+		recs=eval(rd)
+	#bottom-left switch for watermark
+	# !q=[];action.pointer_action.move_to_location(q[0],q[1]);action.pointer_action.click();action.perform()
+	# with open(root+"new"+type+"/","wb") as file: file.write(info.text.encode())
+	for r in recs:
+		if len(r)>3:
+			extra=root+"ex"+type+"/"+r[2]
+			if not os.path.isfile(extra):
+				with open(extra,"w") as file:
+					file.write(r[3])
+		click(r[0],r[1]) #to load for write
+		with open(root+"new"+type+"/"+r[2],"wb") as file: file.write(info.text.encode())
+		action.pointer_action.move_to_location(500,500);action.pointer_action.click();action.perform()
+pack("_new")
+pack("")
 exit(0)
