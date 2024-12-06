22dce4997a3f1c8f3295f9b5a4d9a13a39ec059d
bc <costin.botescu@gmail.com>
2024-12-06 11:11:20 +0200

    see image pixels as text

---
+++ arcgis/pixels.py
@@ -0,0 +1,18 @@
+
+# convert a.png -crop 1720x1030+200+50 a.jpg
+# convert a.jpg -crop 1443x928+110+2 b.jpg
+
+#import numpy
+from PIL import Image
+
+image = Image.open("a.jpg", "r")
+width, height = image.size
+values = list(image.getdata())
+#channels = 3  # image.mode == "RGB"
+#values = numpy.array(values).reshape((width, height, channels))
+
+for y in range(0,height):
+	for x in range(0,width):
+		a=values[width*y+x]
+		print(" %02x/%02x/%02x" % (a[0],a[1],a[2]),end='') #values[x][y][0]
+	print()
