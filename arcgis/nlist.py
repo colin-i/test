c01d3edcbbde95114427b8034aa663abf484740e
bc <costin.botescu@gmail.com>
2024-12-21 06:29:04 +0200

    nlist

--- arcgis/nlist.py
+++ arcgis/nlist.py
@@ -1,8 +1,13 @@
 
 import os
+import sys
 
+if len(sys.argv)>1:
+	z=sys.argv[1]
+else:
+	z=""
 root=os.environ["HOME"]+"/measures/"
-with open(root+"recs_new","rb") as file:
+with open(root+"recs"+z,"rb") as file:
 	recs=eval(file.read())
 	for r in recs:
 		print(r)
