bfd5ff050b10ca86b9ca61552f37804efcf9ec20
bc <costin.botescu@gmail.com>
2025-02-09 13:32:41 +0200

    tab battery

---
+++ plug/t.py
@@ -0,0 +1,35 @@
+#!/data/data/com.termux/files/usr/bin/python
+
+# python /data/data/com.termux/files/usr/lib/python3.12/pdb.py t.py
+
+print('py')
+import sys
+sys.stdout.flush()
+
+# pip install broadlink
+import broadlink
+
+devices = broadlink.discover(discover_ip_address='192.168.1.255')
+device=devices[0]
+device.auth()
+#Authentication failed ? Unlock the device from the mobile app
+
+on=device.check_power()
+
+import subprocess
+import time
+
+while True:
+	a=subprocess.getoutput('termux-battery-status | jq .percentage')
+	print(a)
+	a=int(a)
+	if on:
+		if a>55:
+			device.set_power(False)
+			on=False
+	else:
+		if a<45:
+			device.set_power(True)
+			on=True
+	sys.stdout.flush()
+	time.sleep(100)
