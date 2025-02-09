50ca5879e76085c4bd2c17c1cce4ee9b1b435a02
bc <costin.botescu@gmail.com>
2025-02-09 18:52:17 +0200

    without wait at sleep

--- plug/t.py
+++ plug/t.py
@@ -7,36 +7,35 @@ print('py')
 
 # pip install broadlink / apt install python3-broadlink
 import broadlink
-
-devices = broadlink.discover(discover_ip_address='192.168.1.255')
-device=devices[0]
-device.auth()
-#Authentication failed ? Unlock the device from the mobile app
-
-on=device.check_power()
-
 import subprocess
-import time
-import os
-import sys
-os.set_blocking(sys.stdin.fileno(), False)
+import threading
+exit=threading.Event()
 
-while True:
-	try: #Enter is enough
-		sys.stdin.read(1)[0]
-		break
-	except:
-		pass
-	a=subprocess.getoutput('termux-battery-status | jq .percentage')
-	print(a)
-	a=int(a)
-	if on:
-		if a>55:
-			device.set_power(False)
-			on=False
-	else:
-		if a<45:
-			device.set_power(True)
-			on=True
-	#sys.stdout.flush()
-	time.sleep(100)
+def t_f():
+	devices = broadlink.discover(discover_ip_address='192.168.1.255')
+	device=devices[0]
+	device.auth()
+	#Authentication failed ? Unlock the device from the mobile app
+	on=device.check_power()
+	while not exit.is_set():
+		a=subprocess.getoutput('termux-battery-status | jq .percentage')
+		print(a)
+		a=int(a)
+		if on:
+			if a>55:
+				device.set_power(False)
+				on=False
+		else:
+			if a<45:
+				device.set_power(True)
+				on=True
+		exit.wait(100)
+	print('done')
+
+t = threading.Thread(target=t_f)
+t.start()
+
+import sys
+sys.stdin.read(1)
+#os.set_blocking(sys.stdin.fileno(), False)
+exit.set()
