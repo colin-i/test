50a0d48e41939cd8661cab9a5e52fcc9abcde9a9
bc <costin.botescu@gmail.com>
2025-02-25 06:57:21 +0200

    broadlink hello

--- plug/off.py
+++ plug/off.py
@@ -1,7 +1,11 @@
 
 import broadlink
-
-devices = broadlink.discover(discover_ip_address='192.168.1.255')
-device=devices[0]
-device.auth()
-device.set_power(False)
+import os
+try:
+	with open(os.getenv('HOME')+'/broadlinkip', 'r') as file:
+		dev=broadlink.hello(file.read())
+except:
+	devices = broadlink.discover(discover_ip_address='192.168.1.255')
+	dev=devices[0]
+dev.auth()
+dev.set_power(False)
