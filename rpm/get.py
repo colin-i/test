c9f25be8dd1804702eb04143f49a76949e1503e2
bc <costin.botescu@gmail.com>
2025-05-17 10:31:03 +0300

    copr delete build

--- rpm/get.py
+++ rpm/get.py
@@ -4,5 +4,7 @@ p=sys.argv[1]
 
 from copr.v3 import Client
 client = Client.create_from_config_file()
-x=client.package_proxy.get("colin", "project", p)
+fn=eval("client."+sys.argv[2]+"."+sys.argv[3])
+
+x=fn("colin", "project", p)
 print(x.__response__.json())
