7dc26571521cda3f37a960577af9cfc884e04fc0
up <costin.botescu@gmail.com>
2026-04-05 17:05:31 +0300

    "up"

--- rpm/get.py
+++ rpm/get.py
@@ -2,7 +2,7 @@
 import sys
 p=sys.argv[1]
 
-from copr.v3 import Client #pip install copr
+from copr.v3 import Client # python3-filelock python3-munch # pip install copr
 client = Client.create_from_config_file()
 fn=eval("client."+sys.argv[2]+"."+sys.argv[3])
 
