13466d3ff67240dfc4164ef897a2f8061bd0b790
up <costin.botescu@gmail.com>
2026-06-04 19:43:43 +0300

    "up"

--- arcgis/inc.py
+++ arcgis/inc.py
@@ -1,11 +1,11 @@
 def make_readable(input_filename, output_filename):
     with open(input_filename, 'r', encoding='utf-8') as infile:
         content = infile.read()
-    
+
     # Replace every '<' with a newline followed by '<'
     # .lstrip() ensures we don't accidentally add a blank line at the very beginning
     formatted_content = content.replace('<', '\n<').lstrip()
-    
+
     with open(output_filename, 'w', encoding='utf-8') as outfile:
         outfile.write(formatted_content)
 
