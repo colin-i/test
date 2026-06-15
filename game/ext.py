e8dc4761809b849f7a62fef9d3406dc564718c96
up <costin.botescu@gmail.com>
2026-06-15 16:05:25 +0300

    "up"

--- game/ext.py
+++ game/ext.py
@@ -1,9 +1,13 @@
+
 import subprocess
 import re
+import sys
+import select
 from datetime import datetime
 
 cmd = [
     "journalctl",
+    "--since","now",
     "-f",
     "-o", "cat",
     "/usr/bin/gnome-shell"
@@ -18,9 +22,22 @@ proc = subprocess.Popen(
 
 pattern = re.compile(r"^\[FocusWatch\] extensie$")
 
-for line in proc.stdout:
-    line = line.strip()
+while True:
+    ready, _, _ = select.select([proc.stdout, sys.stdin], [], [])
 
-    if pattern.match(line):
-        now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"Time: {now}")
+    if proc.stdout in ready:
+        line = proc.stdout.readline()
+        if not line:
+            break
+
+        line = line.strip()
+
+        if pattern.match(line):
+            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(f"Time: {now}")
+
+    if sys.stdin in ready:
+        sys.stdin.readline()  # consume Enter
+        print("Enter pressed, exiting.")
+        proc.terminate()
+        break
